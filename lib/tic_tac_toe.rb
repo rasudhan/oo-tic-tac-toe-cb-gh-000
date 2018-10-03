bdc6548152b77a1b11b188d5490eec088e3d51e1
Anandha Sudhan <rasudhan@gmail.com>
2018-10-03 20:08:49 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -72,7 +72,7 @@ class TicTacToe
 
 
 
-  def won?(board)
+  def won?
     WIN_COMBINATIONS.any? do |combo|
      if position_taken?(combo[0]) && @board[combo[0]] == @board[combo[1]] && @board[combo[1]] == @board[combo[2]]
        return combo
