cfeb988568585b5dc9cf6603494b1abd6e0cfb65
Anandha Sudhan <rasudhan@gmail.com>
2018-10-03 20:09:45 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -92,7 +92,7 @@ class TicTacToe
   end
 
   def over?(board)
-    if won?(board) || full?(board) || draw?(board)
+    if won?(board) || draw?(board)
       return true
     else
       return false
